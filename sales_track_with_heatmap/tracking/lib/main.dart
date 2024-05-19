d1ed1f0e30d38f1c073a91c5795c7bb322d2f295
mahbuub-hasan-abid <mahbubhasanabid00@gmail.com>
2024-05-20 00:02:07 +0600

    sales_tracking

--- sales_track_with_heatmap/tracking/lib/main.dart
+++ sales_track_with_heatmap/tracking/lib/main.dart
@@ -20,94 +20,99 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: Colors.black,
-        elevation: 0,
-        leading: CircleAvatar(
-          backgroundImage: AssetImage('assets/profile.jpg'),
-        ),
-        title: Text('David'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/profile.jpg'),
+            ),
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  'Sales per employee per month',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+          title: Text('Hi  Abid'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'Sales per employee per month',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  '263 contributions in the last year',
-                  style: TextStyle(
-                    color: Colors.grey,
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    '263 contributions in the last year',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  YearButton(year: 2021, isSelected: false),
-                  SizedBox(width: 8),
-                  YearButton(year: 2022, isSelected: false),
-                  SizedBox(width: 8),
-                  YearButton(year: 2023, isSelected: true),
-                  SizedBox(width: 8),
-                  YearButton(year: 2024, isSelected: false),
-                ],
-              ),
-              SizedBox(height: 16),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  'Minim dolor in amtni dalia laboris enim dolore consequat.',
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              ContributionCalendar(),
-              SizedBox(height: 16),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    StatCard(
-                      value: '\$4,732.27',
-                      label: 'Last Month',
-                    ),
-                    StatCard(
-                      value: '\$8,034',
-                      label: 'Year to Date',
-                    ),
-                    StatCard(
-                      value: '\$3,124',
-                      label: 'This Month',
-                    ),
+                    YearButton(year: 2021, isSelected: false),
+                    SizedBox(width: 8),
+                    YearButton(year: 2022, isSelected: false),
+                    SizedBox(width: 8),
+                    YearButton(year: 2023, isSelected: true),
+                    SizedBox(width: 8),
+                    YearButton(year: 2024, isSelected: false),
                   ],
                 ),
-              ),
-            ],
+                SizedBox(height: 16),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'Heat map of  sales.',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                ContributionCalendar(),
+                SizedBox(height: 16),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      StatCard(
+                        value: '\$4,732.27',
+                        label: 'Last Month',
+                      ),
+                      StatCard(
+                        value: '\$8,034',
+                        label: 'Year to Date',
+                      ),
+                      StatCard(
+                        value: '\$3,124',
+                        label: 'This Month',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -151,8 +156,6 @@ class YearButton extends StatelessWidget {
 
 class ContributionCalendar extends StatelessWidget {
   final List<String> months = [
-    'Nov',
-    'Dec',
     'Jan',
     'Feb',
     'Mar',
@@ -162,20 +165,24 @@ class ContributionCalendar extends StatelessWidget {
     'Jul',
     'Aug',
     'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: List.generate(
               months.length,
               (index) => Padding(
-                padding: EdgeInsets.only(right: 8),
+                padding: EdgeInsets.only(right: 10),
                 child: Text(
                   months[index],
                   style: TextStyle(
@@ -186,27 +193,28 @@ class ContributionCalendar extends StatelessWidget {
               ),
             ),
           ),
-        ),
-        SizedBox(height: 8),
-        GridView.builder(
-          shrinkWrap: true,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 20,
-            mainAxisSpacing: 4,
-            crossAxisSpacing: 4,
-            childAspectRatio: 0.8,
+          SizedBox(height: 8),
+          GridView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 24,
+              mainAxisSpacing: 4,
+              crossAxisSpacing: 4,
+              childAspectRatio: 0.8,
+            ),
+            itemCount: 360,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: getRandomColor(),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              );
+            },
           ),
-          itemCount: 200,
-          itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                color: getRandomColor(),
-                borderRadius: BorderRadius.circular(4),
-              ),
-            );
-          },
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -214,7 +222,7 @@ class ContributionCalendar extends StatelessWidget {
     final random = Random();
     final colors = [
       Colors.grey[600],
-      Colors.white,
+      Color.fromARGB(255, 228, 223, 223),
       const Color(0xFF0D7517),
     ];
     return colors[random.nextInt(colors.length)];
